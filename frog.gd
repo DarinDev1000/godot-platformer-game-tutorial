99b9750deaaa5ef1f993771ad6be0ccbbd048c8a
DarinDev1000 <darindev1000@gmail.com>
2025-01-21 01:00:59 -0500

    Frog enemy

---
+++ frog.gd
@@ -0,0 +1,54 @@
+extends CharacterBody2D
+
+const SPEED := 50
+var player : CharacterBody2D
+var chase := false
+
+var death := false
+
+func _ready() -> void:
+	get_node("AnimatedSprite2D").play("Idle")
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if !death:
+		if chase:
+			get_node("AnimatedSprite2D").play("Jump")
+			player = $"../../Player2/Player"
+			var direction = (player.position - self.position).normalized()
+			if direction.x > 0:
+				get_node("AnimatedSprite2D").flip_h = true
+			elif direction.x < 0:
+				get_node("AnimatedSprite2D").flip_h = false
+			velocity.x = direction.x * SPEED
+		else:
+			get_node("AnimatedSprite2D").play("Idle")
+			velocity.x = 0
+		move_and_slide()
+
+
+func _on_player_detection_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		chase = true
+
+
+func _on_player_detection_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		chase = false
+
+
+func _on_player_death_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		get_node("AnimatedSprite2D").play("Death")
+		print("Death")
+		death = true
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	print("animation_finished")
+	if death:
+		print("Death after await")
+		self.queue_free()
