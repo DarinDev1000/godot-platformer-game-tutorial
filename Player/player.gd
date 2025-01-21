f2fd0d5907f2005de02ba4b8c9d9ecf821129195
DarinDev1000 <darindev1000@gmail.com>
2025-01-20 23:26:27 -0500

    Animation Player instead of animatedsprite2d

--- Player/player.gd
+++ Player/player.gd
@@ -4,6 +4,12 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animPlayer : AnimationPlayer = get_node("AnimationPlayer")
+@onready var animSprite : AnimatedSprite2D = get_node("AnimatedSprite2D")
+
+func _ready() -> void:
+	pass
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,13 +19,25 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animPlayer.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction < 0:
+		animSprite.flip_h = true
+	elif direction > 0:
+		animSprite.flip_h = false
 	if direction:
 		velocity.x = direction * SPEED
+		if velocity.y == 0:
+			animPlayer.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.y == 0:
+			animPlayer.play("Idle")
+			
+	if velocity.y > 0:
+		animPlayer.play("Fall")
 
 	move_and_slide()
